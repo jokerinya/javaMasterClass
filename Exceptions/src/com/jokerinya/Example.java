582d0209e966d6773cc35e670f2c7026effb8bde
Jokerinya <sakacibrahim@gmail.com>
2021-05-18 01:26:27 +0300

    a neater way of handling exceptions

--- Exceptions/src/com/jokerinya/Example.java
+++ Exceptions/src/com/jokerinya/Example.java
@@ -6,24 +6,20 @@ import java.util.Scanner;
 
 public class Example {
     public static void main(String[] args) {
-        int result = divide();
-        System.out.println(result);
+        try{
+            int result = divide();
+            System.out.println(result);
+        } catch (ArithmeticException | NoSuchElementException e){
+            System.out.println(e.toString());
+            System.out.println("Something went wrong, program closed.");
+        }
     }
 
     public static int divide() {
         int x, y;
-        try {
-            x = getInt();
-            y = getInt();
-        } catch (NoSuchElementException e ){
-            throw new ArithmeticException("No suitable input");
-        }
-
-        try {
-            return x / y;
-        } catch (ArithmeticException e){
-            throw new ArithmeticException("Attempt to divide to zero");
-        }
+        x = getInt();
+        y = getInt();
+        return x / y;
     }
 
     public static int getInt(){
